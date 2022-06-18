ad11cfc0978d9205e7644cc86238ca93ad61a8be
Stephen Reindl <stephen.reindl@outlook.de>
2022-06-18 18:25:51 +0200

    feat: api contains web now

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,12 +1,21 @@
 import * as express from 'express';
+import * as path from 'path';
 import { Message } from '@stevenr/api-interfaces';
 
+const CLIENT_BUILD_PATH = path.join(__dirname, '../bb-monitor');
+
 const app = express();
+
+app.use(express.static(CLIENT_BUILD_PATH));
 
 const greeting: Message = { message: 'Welcome to api!' };
 
 app.get('/api', (req, res) => {
   res.send(greeting);
+});
+
+app.get('*', (request, response) => {
+  response.sendFile(path.join(CLIENT_BUILD_PATH, 'index.html'));
 });
 
 const port = process.env.port || 3333;
