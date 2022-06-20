0dade73832b0cf79ae63faf6c8253a9b723cd43d
Stephen Reindl <stephen.reindl@outlook.de>
2022-06-20 20:13:32 +0200

    feat(ui): new landing page

--- apps/bb-monitor/src/app/components/page-header/index.tsx
+++ apps/bb-monitor/src/app/components/page-header/index.tsx
@@ -1,46 +1,49 @@
-import { FC } from "react";
-import { SpaceProps } from "@stevenr/shared";
-import Breadcrumb from "../breadcrumb";
-import SearchForm from "../search-form";
-import { StyledWrap } from "./style";
+import { FC } from 'react';
+import { SpaceProps } from '@stevenr/shared';
+import Breadcrumb from '../breadcrumb';
+import SearchForm from '../search-form';
+import { StyledWrap } from './style';
 
 interface IProps extends SpaceProps {
-    /**
-     * Pass previous page's text and link
-     */
-    prev: Array<{
-        text: string;
-        link: string;
-    }>;
-    /**
-     * Write page's title
-     */
-    title: string;
-    /**
-     * Write page's welcome text
-     */
-    wcText?: string;
-    /**
-     * Put Extra classes
-     */
-    className?: string;
+  /**
+   * Pass previous page's text and link
+   */
+  prev: Array<{
+    text: string;
+    link: string;
+  }>;
+  /**
+   * Write page's title
+   */
+  title: string;
+  /**
+   * Write page's welcome text
+   */
+  wcText?: string;
+  /**
+   * Put Extra classes
+   */
+  className?: string;
+  /** display search form  */
+  haveSearch?: boolean;
 }
 
 const PageHeader: FC<IProps> = ({
-    prev,
-    title,
-    wcText,
-    className,
-    ...rest
+  prev,
+  title,
+  wcText,
+  className,
+  haveSearch,
+  ...rest
 }) => {
-    return (
-        <StyledWrap className={className} {...rest}>
-            <div>
-                <Breadcrumb prev={prev} title={title} wcText={wcText} />
-            </div>
-            <SearchForm />
-        </StyledWrap>
-    );
+  return (
+    <StyledWrap className={className} {...rest}>
+      <div>
+        <Breadcrumb prev={prev} title={title} wcText={wcText} />
+      </div>
+      {haveSearch && <SearchForm />}
+    </StyledWrap>
+  );
 };
 
 export default PageHeader;
